3f88c13de2951a01fc44362c3a8f6c7a0f0dbd88
DommarajuUmamaheswari <53852877+DommarajuUmamaheswari@users.noreply.github.com>
2020-02-19 09:54:49 +0530

    Add files via upload

---
+++ WebApplication1/Controllers/LogicController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Momentum.External.Api.Controllers
+{
+    [ApiController]
+    [Route("[action]")]
+    public class LogicController : ControllerBase
+    {
+        private readonly ILogger<LogicController> _logger;
+
+        public LogicController(ILogger<LogicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Implementation pending";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Implementation pending";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Implementation pending";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Implementation pending";
+        }
+    }
+}
